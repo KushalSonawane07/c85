ffcc8aae09c76d48048da0615157dfba494c2ded
KushalSonawane07 <72611496+KushalSonawane07@users.noreply.github.com>
2022-04-24 17:36:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+ export const firebaseConfig = {
+    apiKey: "AIzaSyClemtbTpPy3zSimThJn-ZIDrKLTVl_oMo",
+    authDomain: "storytelllingapp-26fbe.firebaseapp.com",
+    projectId: "storytelllingapp-26fbe",
+    storageBucket: "storytelllingapp-26fbe.appspot.com",
+    messagingSenderId: "287559463483",
+    appId: "1:287559463483:web:1eab589da838ec72dc221e",
+    measurementId: "G-0DTWW0LDBQ"
+  };
